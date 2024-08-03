9e4a31ecf44d59cf3e9b7956ee6033af8dee2681
iloveryuux <martiin17jr@gmail.com>
2024-08-03 07:31:01 +0200

    feat: add colors and new loglevels

--- src/logger.ts
+++ src/logger.ts
@@ -1,10 +1,39 @@
+import chalk from 'chalk'
+
+import { BaseConfig, DefaultConfig } from './types.js'
+
 class Logger {
+  private config: BaseConfig
+
+  constructor(config: BaseConfig = DefaultConfig) {
+    this.config = { ...DefaultConfig, ...config }
+  }
+
+  private format(message: string, color): string {
+    if (this.config.colors) {
+      message = color(message)
+    }
+    return message
+  }
+
   log(message: string): void {
-    console.log(message)
+    console.log(this.format(message, chalk.green))
   }
 
   error(message: string): void {
-    console.error(message)
+    console.error(this.format(message, chalk.red))
+  }
+
+  info(message: string): void {
+    console.info(this.format(message, chalk.blue))
+  }
+
+  warn(message: string): void {
+    console.warn(this.format(message, chalk.yellow))
+  }
+
+  debug(message: string): void {
+    console.debug(this.format(message, chalk.gray))
   }
 }
 
---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export interface BaseConfig {
+  colors?: boolean
+}
+
+export const DefaultConfig: BaseConfig = {
+  colors: true
+}
